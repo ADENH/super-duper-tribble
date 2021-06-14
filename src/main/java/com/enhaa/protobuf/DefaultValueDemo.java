b7851ab3c38e24abbd0d581458ab6af38bfa579a
adenurhidayat <ade.hidayat@axa.co.id>
2021-06-14 14:36:13 +0700

    importing module

---
+++ src/main/java/com/enhaa/protobuf/DefaultValueDemo.java
@@ -0,0 +1,11 @@
+package com.enhaa.protobuf;
+
+import com.protobuf.models.Person;
+
+public class DefaultValueDemo {
+
+	public static void main(String[] args) {
+		
+		Person person = Person.newBuilder().build();
+	}
+}
